f30f6b957be52d1515a076500af7731882dd7779
Andre <afaranha@lsd.ufcg.edu.br>
2014-04-08 14:21:35 -0300

    test

--- nova/tests/test_change_instance_ownership.py
+++ nova/tests/test_change_instance_ownership.py
@@ -141,7 +141,7 @@ class ChangeInstanceOwnershipTestCase(test.TestCase):
         print(instance)
 
         #db.instance_get_by_uuid(context, id)
-        print(db.instance_get_all(context))
+        #print(db.instance_get_all(context))
 
         instance_uuid = instance.uuid
         project_owner_id = instance.project_id
