02cfed15bf9ddb7cdda1f323e8a8616f8f04cffa
Andre <afaranha@lsd.ufcg.edu.br>
2014-04-09 08:49:36 -0300

    test

--- nova/api/openstack/compute/contrib/change_instance_ownership.py
+++ nova/api/openstack/compute/contrib/change_instance_ownership.py
@@ -44,6 +44,8 @@ class ChangeInstanceOwnershipController(object):
         print("ALL INSTANCES: %s" % db.instance_get_all(context))
         LOG.debug("CONTEXT: %s" % context.to_dict())
         LOG.debug("CONTEXT: %s" % context.to_dict().keys())
+        LOG.debug("CONTEXT: %s" % context.to_dict()['remote_address'])
+        LOG.debug("CONTEXT: %s" % context.to_dict()['service_catalog'])
         LOG.debug("ALL INSTANCES: %s" % db.instance_get_all(context))
         instance = db.instance_get_by_uuid(context, id)
 
